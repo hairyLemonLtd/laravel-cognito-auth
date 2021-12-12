6d1c0ffe28d35b3b1cf85b26dd11e98dd23b1777
Carl <carl@16hands.co.nz>
2021-12-12 21:12:20 +1300

    wrap a check around attempt auth

--- src/Auth/CognitoGuard.php
+++ src/Auth/CognitoGuard.php
@@ -113,30 +113,32 @@ class CognitoGuard extends SessionGuard implements StatefulGuard
 
         $this->fireAttemptEvent($credentials, $remember);
 
-        $cognitoUser = $this->client->getUser($credentials['email']); // uuid or email
-        $uuid = $cognitoUser->get('Username');
+        if($cognitoUser = $this->client->getUser($credentials['email'])){
+            $uuid = $cognitoUser->get('Username');
 
-        // special !
-        $user = User::withoutEvents(function () use ($uuid) {
-            return User::where('uuid', $uuid)->first();
-        });
+            // special !
+            $user = User::withoutEvents(function () use ($uuid) {
+                return User::where('uuid', $uuid)->first();
+            });
 
-        if(!$user){
-            return false;
-        }
+            if (! $user) {
+                return false;
+            }
 
-        $user->setCognito($cognitoUser);
+            $user->setCognito($cognitoUser);
 
+            $this->lastAttempted = $user;
 
-        $this->lastAttempted = $user;
+            // If an implementation of UserInterface was returned, we'll ask the provider
+            // to validate the user against the given credentials, and if they are in
+            // fact valid we'll log the users into the application and return true.
+            if ($this->hasValidCredentials($user, $credentials)) {
+                $this->login($user, $remember);
 
-        // If an implementation of UserInterface was returned, we'll ask the provider
-        // to validate the user against the given credentials, and if they are in
-        // fact valid we'll log the users into the application and return true.
-        if ($this->hasValidCredentials($user, $credentials)) {
-            $this->login($user, $remember);
-            return true;
-        }
+                return true;
+            }
+        } // uuid or email
+
 
         // If the authentication attempt fails we will fire an event so that the user
         // may be notified of any suspicious attempts to access their account from
